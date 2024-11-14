03c7770d0fa8b8aa6cc60848568ed6bbecdefc24
AMRWERY <amrmounir2@gmail.com>
2024-11-14 18:48:51 +0200

    .

--- components/home-components/on-instagram.vue
+++ components/home-components/on-instagram.vue
@@ -29,7 +29,7 @@
       </template>
     </Carousel>
 
-    <div class="px-6 mx-auto my-6 max-w-7xl lg:px-8">
+    <div class="px-6 mx-auto mb-6 mt-10 py-4 max-w-7xl lg:px-8 bg-[#fafafa]">
       <div
         class="grid items-center max-w-lg grid-cols-4 mx-auto gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-5 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-5">
         <img class="object-contain w-full col-span-2 max-h-12 lg:col-span-1"
