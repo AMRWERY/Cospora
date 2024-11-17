d0d111c9f52b274ce2de6ce1b708fe8f0c3d5585
AMRWERY <amrmounir2@gmail.com>
2024-11-17 10:52:37 +0200

    add underline with animation to cards titles

--- components/home-components/carousel-component.vue
+++ components/home-components/carousel-component.vue
@@ -110,8 +110,11 @@
                 <div class="relative w-full h-48 overflow-hidden rounded-lg group-hover:opacity-75 sm:h-48">
                     <img :src="card.img" class="object-cover object-center size-full" />
                     <div class="absolute inset-0 flex items-center justify-center">
-                        <h3 class="p-2 text-xl font-semibold text-white uppercase">
+                        <h3 class="relative overflow-hidden text-xl font-semibold text-white uppercase">
                             {{ card.title }}
+                            <span
+                                class="absolute bottom-0 left-0 w-0 h-0.5 bg-white transition-all duration-300 group-hover:w-full">
+                            </span>
                         </h3>
                     </div>
                 </div>
