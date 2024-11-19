ab49c8c4926ef0a6501ec8d5a5ef8911e95e32e7
AMRWERY <amrmounir2@gmail.com>
2024-11-19 15:06:43 +0200

    handle care page and may-also-like component

--- components/home-components/featured-products.vue
+++ components/home-components/featured-products.vue
@@ -7,66 +7,67 @@
     </div>
 
     <ClientOnly>
-    <Carousel v-bind="config">
-      <Slide v-for="(card, index) in products" :key="index">
-        <div class="carousel__item">
-          <div class="relative flex flex-col w-full max-w-xs my-10 overflow-hidden bg-white group">
-            <nuxt-link class="relative flex mx-3 mt-3 overflow-hidden h-60 rounded-xl" to="">
-              <div class="relative w-full h-full">
-                <img class="absolute top-0 right-0 object-cover w-full h-full transition-all duration-500 ease-in-out"
-                  :src="card.imgOne" />
-                <img
-                  class="absolute top-0 right-0 object-cover w-full h-full transition-all duration-500 ease-in-out opacity-0 group-hover:opacity-100"
-                  :src="card.imgTwo" />
-              </div>
-              <div class="absolute px-2 py-1 text-xs text-white bg-opacity-50 top-2 left-2">
-                <p v-if="card.sale" class="px-2 py-1 text-white bg-red-500 rounded">Sale</p>
-                <p class="px-2 py-1 mt-1 text-white bg-black rounded" v-if="card.new">New</p>
-              </div>
-            </nuxt-link>
-
-            <button type="button" class="absolute z-10 mt-5 rounded-full top-2 right-5"
-              @click.stop="toggleFavorite(index)">
-              <icon :name="isFavorite[index] ? 'heroicons-solid:heart' : 'heroicons-outline:heart'" class="w-5 h-5"
-                :class="isFavorite[index] ? 'text-red-500' : 'text-gray-500'" />
-            </button>
-
-            <div
-              class="absolute inset-0 flex items-center justify-center transition-opacity opacity-0 mb-28 group-hover:opacity-100">
-              <new-products-dialog />
-            </div>
-
-            <div class="px-5 pb-5 mt-4">
-              <nuxt-link to="">
-                <h5 class="text-xs font-bold tracking-tight capitalize text-slate-900">
-                  {{ card.title }}
-                </h5>
-                <p>{{ card.subtitle }}</p>
+      <Carousel v-bind="config">
+        <Slide v-for="(card, index) in products" :key="index">
+          <div class="carousel__item">
+            <div class="relative flex flex-col w-full max-w-xs my-10 overflow-hidden bg-white group">
+              <nuxt-link class="relative flex mx-3 mt-3 overflow-hidden h-60 rounded-xl" to="">
+                <div class="relative w-full h-full">
+                  <img class="absolute top-0 right-0 object-cover w-full h-full transition-all duration-500 ease-in-out"
+                    :src="card.imgOne" />
+                  <img
+                    class="absolute top-0 right-0 object-cover w-full h-full transition-all duration-500 ease-in-out opacity-0 group-hover:opacity-100"
+                    :src="card.imgTwo" />
+                </div>
+                <div class="absolute px-2 py-1 text-xs text-white bg-opacity-50 top-2 left-2">
+                  <p v-if="card.sale" class="px-2 py-1 text-white bg-red-500 rounded">Sale</p>
+                  <p class="px-2 py-1 mt-1 text-white bg-black rounded" v-if="card.new">New</p>
+                </div>
               </nuxt-link>
-              <div class="flex flex-col items-center justify-between mt-2 mb-5 font-semibold text-center">
-                <p>
-                  <span class="mr-2 text-sm text-gray-400 line-through" v-if="card.discount">${{ card.discount }}</span>
-                  <span class="text-sm text-red-700">${{ card.price }}</span>
-                </p>
+
+              <button type="button" class="absolute z-10 mt-5 rounded-full top-2 right-5"
+                @click.stop="toggleFavorite(index)">
+                <icon :name="isFavorite[index] ? 'heroicons-solid:heart' : 'heroicons-outline:heart'" class="w-5 h-5"
+                  :class="isFavorite[index] ? 'text-red-500' : 'text-gray-500'" />
+              </button>
+
+              <div
+                class="absolute inset-0 flex items-center justify-center transition-opacity opacity-0 mb-28 group-hover:opacity-100">
+                <new-products-dialog />
               </div>
 
-              <!-- Select Options Button -->
-              <div class="relative transition-opacity opacity-0 group-hover:opacity-100">
-                <button type="button"
-                  class="flex items-center justify-center rounded-md bg-slate-900 px-5 py-2.5 text-center text-sm text-white hover:bg-gray-700 focus:bg-red-500 focus:text-white focus:outline-none focus:ring-4 focus:ring-blue-300 w-full capitalize font-semibold">
-                  Select Options
-                </button>
+              <div class="px-5 pb-5 mt-4">
+                <nuxt-link to="">
+                  <h5 class="text-xs font-bold tracking-tight capitalize text-slate-900">
+                    {{ card.title }}
+                  </h5>
+                  <p>{{ card.subtitle }}</p>
+                </nuxt-link>
+                <div class="flex flex-col items-center justify-between mt-2 mb-5 font-semibold text-center">
+                  <p>
+                    <span class="mr-2 text-sm text-gray-400 line-through" v-if="card.discount">${{ card.discount
+                      }}</span>
+                    <span class="text-sm text-red-700">${{ card.price }}</span>
+                  </p>
+                </div>
+
+                <!-- Select Options Button -->
+                <div class="relative transition-opacity opacity-0 group-hover:opacity-100">
+                  <button type="button"
+                    class="flex items-center justify-center rounded-md bg-slate-900 px-5 py-2.5 text-center text-sm text-white hover:bg-gray-700 focus:bg-red-500 focus:text-white focus:outline-none focus:ring-4 focus:ring-blue-300 w-full capitalize font-semibold">
+                    Select Options
+                  </button>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      </Slide>
+        </Slide>
 
-      <template #addons>
-        <Navigation />
-      </template>
-    </Carousel>
-  </ClientOnly>
+        <template #addons>
+          <Navigation />
+        </template>
+      </Carousel>
+    </ClientOnly>
   </div>
 </template>
 
---
+++ components/may-also-like.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+    <div class="block xs:mt-8 xl:block">
+      <h3 class="text-2xl font-semibold text-gray-900 dark:text-white">You may also like</h3>
+      <div class="grid gap-4 mt-6 sm:mt-8 sm:grid-cols-2 lg:grid-cols-3">
+
+        <div v-for="product in products" :key="product"
+          class="p-3 space-y-6 overflow-hidden bg-white border border-gray-200 rounded-lg shadow-sm dark:border-gray-700 dark:bg-gray-800">
+          <nuxt-link to="" class="relative flex overflow-hidden rounded group">
+            <div class="relative mx-auto h-44 w-44">
+              <img class="absolute top-0 right-0 object-cover transition-all duration-500 ease-in-out h-44 w-44"
+                :src="product.imgOne" />
+              <img
+                class="absolute top-0 right-0 object-cover transition-all duration-500 ease-in-out opacity-0 h-44 w-44 group-hover:opacity-100"
+                :src="product.imgTwo" />
+            </div>
+            <div class="absolute px-2 py-1 text-xs text-white bg-opacity-50 top-2 left-2">
+              <p v-if="product.sale" class="px-2 py-1 text-white bg-red-500 rounded">Sale</p>
+              <p class="px-2 py-1 mt-1 text-white bg-black rounded" v-if="product.new">New</p>
+            </div>
+          </nuxt-link>
+          <div>
+            <nuxt-link to=""
+              class="text-lg font-semibold leading-tight text-gray-900 hover:underline dark:text-white">{{ product.title
+              }}</nuxt-link>
+            <p class="mt-2 text-base font-normal text-gray-500 dark:text-gray-400">
+              {{ product.subtitle }}
+            </p>
+          </div>
+          <div>
+            <p class="text-lg font-bold text-gray-900 dark:text-white">
+              <span class="line-through">${{ product.price }}</span>
+            </p>
+            <p class="text-lg font-bold leading-tight text-red-600 dark:text-red-500">${{ product.discount }}</p>
+          </div>
+          <div class="mt-6 flex items-center gap-2.5">
+            <button data-tooltip-target="favourites-tooltip-1" type="button"
+              class="inline-flex items-center justify-center gap-2 rounded-lg border border-gray-200 bg-white p-2.5 text-sm font-medium text-gray-900 hover:bg-gray-100 hover:text-primary-700 focus:z-10 focus:outline-none focus:ring-4 focus:ring-gray-100 dark:border-gray-600 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white dark:focus:ring-gray-700">
+              <icon name="heroicons-outline:heart" class="w-5 h-5" />
+            </button>
+            <button type="button"
+              class="inline-flex w-full items-center justify-center rounded-lg bg-primary-700 px-5 py-2.5 text-sm font-medium text-white hover:bg-primary-800 focus:outline-none focus:ring-4 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
+              <icon name="material-symbols-light:add-shopping-cart-rounded" class="w-5 h-5 me-0.5" />
+              Add to cart
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const products = ref([
+  { imgOne: 'https://justfields.com/storage/projects/7M5rV059/product-01.jpg', imgTwo: 'https://justfields.com/storage/projects/7M5rV059/product-02.jpg', sale: true, new: true, title: 'Chanel', subtitle: 'Ainterdu pretium de miancelos dincidunts', price: '89.00', discount: '39.00' },
+  { imgOne: 'https://justfields.com/storage/projects/7M5rV059/product-03.jpg', imgTwo: 'https://justfields.com/storage/projects/7M5rV059/product-04.jpg', sale: true, new: true, title: 'Chanel', subtitle: 'Baminos lementum disus an cras damos dincidunts', price: '89.00', discount: '39.00' },
+  { imgOne: 'https://justfields.com/storage/projects/7M5rV059/product-09.jpg', imgTwo: 'https://justfields.com/storage/projects/7M5rV059/product-010.jpg', sale: false, new: true, title: 'Jimmy Choo', subtitle: 'Dinterdum pretium de milancelos cras dincidunts', price: '89.00', discount: '39.00' },
+])
+</script>
