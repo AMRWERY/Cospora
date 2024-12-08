40851792a7caf41b3be58635d8d1d334750c2545
AMRWERY <amrmounir2@gmail.com>
2024-12-08 19:32:10 +0200

    .

--- stores/productsStore.js
+++ stores/productsStore.js
@@ -4,38 +4,38 @@ import { collection, getDocs, addDoc } from "firebase/firestore";
 
 export const useProductsStore = defineStore("products", {
   state: () => ({
-    // categories: [],
-    // subCategories: [],
+    categories: [],
+    subCategories: [],
     image1Url: "",
     image2Url: "",
   }),
 
   actions: {
-    // async fetchCategories() {
-    //   try {
-    //     const querySnapshot = await getDocs(collection(db, "categories"));
-    //     this.categories = querySnapshot.docs.map((doc) => ({
-    //       id: doc.id,
-    //       ...doc.data(),
-    //     }));
-    //     // console.log("Fetched categories:", this.categories);
-    //   } catch (error) {
-    //     console.error("Error fetching categories:", error);
-    //   }
-    // },
+    async fetchCategories() {
+      try {
+        const querySnapshot = await getDocs(collection(db, "categories"));
+        this.categories = querySnapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+        // console.log("Fetched categories:", this.categories);
+      } catch (error) {
+        console.error("Error fetching categories:", error);
+      }
+    },
 
-    // async fetchSubCategories() {
-    //   try {
-    //     const querySnapshot = await getDocs(collection(db, "subCategories"));
-    //     this.subCategories = querySnapshot.docs.map((doc) => ({
-    //       id: doc.id,
-    //       ...doc.data(),
-    //     }));
-    //     // console.log("Fetched subcategories:", this.subCategories);
-    //   } catch (error) {
-    //     console.error("Error fetching subcategories:", error);
-    //   }
-    // },
+    async fetchSubCategories() {
+      try {
+        const querySnapshot = await getDocs(collection(db, "subCategories"));
+        this.subCategories = querySnapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+        // console.log("Fetched subcategories:", this.subCategories);
+      } catch (error) {
+        console.error("Error fetching subcategories:", error);
+      }
+    },
 
     async createProduct(productData) {
       try {
