ce9db5cecb613bd358a9ba355bb283342f7c440f
AMRWERY <amrmounir2@gmail.com>
2024-12-27 21:02:58 +0200

    .

--- components/charts.vue
+++ components/charts.vue
@@ -1,25 +1,84 @@
 <template>
   <div>
-    <!-- <client-only> -->
-    <apexchart type="bar" :options="chartOptions" :series="chartSeries" />
-    <!-- </client-only> -->
+    <div id="chart">
+      <apexchart type="area" height="350" :options="chartOptions" :series="series"></apexchart>
+    </div>
   </div>
 </template>
 
 <script setup>
-const chartOptions = ref({
-  chart: {
-    id: 'example',
-  },
-  xaxis: {
-    categories: ['Jan', 'Feb', 'Mar'],
-  },
-});
+const generateDayWiseTimeSeries = (baseval, count, yrange) => {
+  let i = 0;
+  const series = [];
+  while (i < count) {
+    const x = baseval;
+    const y =
+      Math.floor(Math.random() * (yrange.max - yrange.min + 1)) + yrange.min;
+    series.push({ x, y });
+    baseval += 86400000;
+    i++;
+  }
+  return series;
+};
 
-const chartSeries = ref([
+const series = ref([
   {
-    name: 'Sales',
-    data: [30, 40, 50],
+    name: "South",
+    data: generateDayWiseTimeSeries(
+      new Date("11 Feb 2017 GMT").getTime(),
+      20,
+      { min: 10, max: 60 }
+    ),
+  },
+  {
+    name: "North",
+    data: generateDayWiseTimeSeries(
+      new Date("11 Feb 2017 GMT").getTime(),
+      20,
+      { min: 10, max: 20 }
+    ),
+  },
+  {
+    name: "Central",
+    data: generateDayWiseTimeSeries(
+      new Date("11 Feb 2017 GMT").getTime(),
+      20,
+      { min: 10, max: 15 }
+    ),
   },
 ]);
+
+const chartOptions = ref({
+  chart: {
+    type: "area",
+    height: 350,
+    stacked: true,
+    events: {
+      selection: function (chart, e) {
+        console.log(new Date(e.xaxis.min));
+      },
+    },
+  },
+  colors: ["#008FFB", "#00E396", "#CED4DC"],
+  dataLabels: {
+    enabled: false,
+  },
+  stroke: {
+    curve: "monotoneCubic",
+  },
+  fill: {
+    type: "gradient",
+    gradient: {
+      opacityFrom: 0.6,
+      opacityTo: 0.8,
+    },
+  },
+  legend: {
+    position: "top",
+    horizontalAlign: "left",
+  },
+  xaxis: {
+    type: "datetime",
+  },
+});
 </script>
