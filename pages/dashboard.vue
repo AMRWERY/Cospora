8b36d95165b7a60820cb77a53c1819e43b36b6da
AMRWERY <amrmounir2@gmail.com>
2024-12-27 19:54:46 +0200

    .

--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -1,12 +1,12 @@
 <template>
   <div>
     <div class="container p-2 mx-auto">
-      <div class="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-4">
-        <!-- Card 1 - Sales -->
+      <div class="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3">
+        <!-- Card 1 - Orders -->
         <div class="p-4 bg-white rounded-lg shadow-md">
-          <h3 class="text-xl font-semibold text-center">Total Sales This Month</h3>
+          <h3 class="text-xl font-semibold text-center">Orders Placed</h3>
           <div class="flex items-center justify-between mt-4">
-            <icon name="material-symbols:attach-money" class="text-gray-700 h-7 w-7" />
+            <icon name="material-symbols:shopping-cart-checkout" class="text-gray-700 h-7 w-7" />
             <p class="text-xl font-semibold text-gray-700">{{ totalCheckouts }}</p>
           </div>
           <div class="mt-6 text-center">
@@ -14,15 +14,16 @@
           </div>
         </div>
 
-        <!-- Card 2 - Orders -->
+        <!-- Card 2 - Inventory -->
         <div class="p-4 bg-white rounded-lg shadow-md">
-          <h3 class="text-xl font-semibold text-center">Orders Placed</h3>
+          <h3 class="text-xl font-semibold text-center">Inventory</h3>
           <div class="flex items-center justify-between mt-4">
-            <icon name="material-symbols:shopping-cart-checkout" class="text-gray-700 h-7 w-7" />
-            <p class="text-xl font-semibold text-gray-700">20</p>
+            <icon name="lucide:boxes" class="text-gray-700 h-7 w-7" />
+            <p class="text-xl font-semibold text-gray-700">{{ inStockCount }}</p>
           </div>
           <div class="mt-6 text-center">
-            <nuxt-link to="" type="button" class="px-4 py-2 text-white bg-blue-500 rounded-md">View Details</nuxt-link>
+            <nuxt-link to="/products" type="button" class="px-4 py-2 text-white bg-blue-500 rounded-md">View
+              Details</nuxt-link>
           </div>
         </div>
 
@@ -38,22 +39,8 @@
               Details</nuxt-link>
           </div>
         </div>
-
-        <!-- Card 4 - Inventory -->
-        <div class="p-4 bg-white rounded-lg shadow-md">
-          <h3 class="text-xl font-semibold text-center">Inventory</h3>
-          <div class="flex items-center justify-between mt-4">
-            <icon name="lucide:boxes" class="text-gray-700 h-7 w-7" />
-            <p class="text-xl font-semibold text-gray-700">{{ inStockCount }}</p>
-          </div>
-          <div class="mt-6 text-center">
-            <nuxt-link to="/products" type="button" class="px-4 py-2 text-white bg-blue-500 rounded-md">View
-              Details</nuxt-link>
-          </div>
-        </div>
       </div>
     </div>
-
   </div>
 </template>
 
