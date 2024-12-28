3989085415148ae792735705694ad9784988e5da
AMRWERY <amrmounir2@gmail.com>
2024-12-28 16:33:20 +0200

    .

--- pages/order-summary.vue
+++ pages/order-summary.vue
@@ -1,18 +1,18 @@
 <template>
   <div>
     <breadcrumb />
-    
+
     <div class="max-w-4xl p-6 mx-auto my-10 bg-white rounded-lg shadow-lg" id="Cospora">
       <!-- Order Number -->
       <div class="flex items-center justify-between mb-12">
         <div>
-          <h2 class="text-2xl font-semibold text-gray-900">Order Summary</h2>
+          <h2 class="text-2xl font-semibold text-gray-900">{{ $t('order_summary.order_summary') }}</h2>
           <p class="mt-1 text-sm text-gray-500">
-            Order Number: <span class="font-semibold text-gray-900">{{ orderId }}</span>
+            {{ $t('order_summary.order_number') }} <span class="font-semibold text-gray-900">{{ orderId }}</span>
           </p>
         </div>
         <button @click="downloadPDF" data-twe-toggle="tooltip" data-twe-placement="top" data-twe-ripple-init
-          title="Download PDF"
+          :title="$t('tooltip.download_pdf')"
           class="flex items-center px-4 py-4 text-blue-700 border border-blue-700 rounded-full hover:bg-blue-100">
           <icon name="icon-park:printer" class="w-5 h-5" />
         </button>
@@ -25,14 +25,15 @@
         </div>
         <div class="col-span-10">
           <p class="text-lg font-semibold text-gray-900">{{ item.title }}</p>
-          <p class="text-sm font-semibold text-gray-700"><span>Category: </span>{{ item.categoryTitle }}</p>
+          <p class="text-sm font-semibold text-gray-700"><span class="me-1">{{ $t('order_summary.category') }} </span>{{
+            item.categoryTitle }}</p>
         </div>
       </div>
 
       <!-- Subtotal Section -->
       <div class="grid grid-cols-12 gap-6 pb-6 mb-6 border-b">
         <div class="col-span-8">
-          <dt class="text-sm font-medium text-gray-500">Subtotal</dt>
+          <dt class="text-sm font-medium text-gray-500">{{ $t('order_summary.subtotal') }}</dt>
         </div>
         <div class="col-span-4 text-right">
           <dd class="text-sm font-medium text-gray-900">${{ subTotalAmount }}</dd>
@@ -42,7 +43,7 @@
       <!-- Savings Section -->
       <div class="grid grid-cols-12 gap-6 pb-6 mb-6 border-b">
         <div class="col-span-8">
-          <dt class="text-sm font-medium text-gray-500">Savings</dt>
+          <dt class="text-sm font-medium text-gray-500">{{ $t('order_summary.savings') }}</dt>
         </div>
         <div class="col-span-4 text-right">
           <dd class="text-sm font-medium text-green-500">%{{ averageDiscount }}</dd>
@@ -52,7 +53,7 @@
       <!-- Store Pickup Section -->
       <div class="grid grid-cols-12 gap-6 pb-6 mb-6 border-b">
         <div class="col-span-8">
-          <dt class="text-sm font-medium text-gray-500">Store Pickup</dt>
+          <dt class="text-sm font-medium text-gray-500">{{ $t('order_summary.store_pickup') }}</dt>
         </div>
         <div class="col-span-4 text-right">
           <dd class="text-sm font-medium text-gray-900">$25.00</dd>
@@ -62,7 +63,7 @@
       <!-- Tax Section -->
       <div class="grid grid-cols-12 gap-6 pb-6 mb-6 border-b">
         <div class="col-span-8">
-          <dt class="text-sm font-medium text-gray-500">Tax</dt>
+          <dt class="text-sm font-medium text-gray-500">{{ $t('order_summary.tax') }}</dt>
         </div>
         <div class="col-span-4 text-right">
           <dd class="text-sm font-medium text-gray-900">$18.00</dd>
@@ -72,7 +73,7 @@
       <!-- Total Section -->
       <div class="grid grid-cols-12 gap-6">
         <div class="col-span-8 font-semibold text-gray-900">
-          <dt class="text-xl">Total</dt>
+          <dt class="text-xl">{{ $t('order_summary.total') }}</dt>
         </div>
         <div class="col-span-4 text-right">
           <dd class="text-xl font-semibold text-gray-900">${{ totalAmount }}</dd>
@@ -157,6 +158,12 @@ const downloadPDF = () => {
     html2pdf().from(pdfContent).set(options).save();
   }
 };
+
+const { t } = useI18n()
+
+useHead({
+  titleTemplate: () => t("head.order_summary"),
+});
 
 onMounted(async () => {
   const { Tooltip, initTWE } = await import("tw-elements");
