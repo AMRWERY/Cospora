34ff18c8839d03fea6b5450636c6f0ec50ccf68f
AMRWERY <amrmounir2@gmail.com>
2024-12-29 14:11:19 +0200

    .

--- components/home-components/carousel-component.vue
+++ components/home-components/carousel-component.vue
@@ -20,18 +20,18 @@
             <!--Carousel items-->
             <div class="relative w-full overflow-hidden after:clear-both after:block after:content-['']">
                 <!--First item-->
-                <div class="relative float-left -me-[100%] w-full transition-transform duration-[600ms] ease-in-out motion-reduce:transition-none"
+                <div class="relative float-start -me-[100%] w-full transition-transform duration-[600ms] ease-in-out motion-reduce:transition-none"
                     data-twe-carousel-item data-twe-carousel-active>
                     <img src="https://justfields.com/storage/projects/7M5rV059/home-01.jpg"
                         class="block mx-auto w-7xl" />
                     <div
-                        class="absolute inset-y-0 flex flex-col items-center justify-center w-1/3 space-y-2 text-lg font-semibold text-center text-black ms-36 start-5">
+                        class="absolute inset-y-0 flex flex-col items-center justify-center w-1/3 space-y-2 text-lg font-semibold text-center text-black ms-36 left-10">
                         <p class="text-lg font-semibold capitalize lg:text-4xl">
                             {{ $t('home.milancelos') }}
                         </p>
                         <p class="hidden text-xs font-light line-clamp-2 sm:block">{{
                             $t('home.pellentesque_posuere_morci_lobortis_scelerisque_blandit_donec_an_tellus_lacinia_an_tincidunt_risus_de_cosmopolis_consequat_velitos')
-                        }}</p>
+                            }}</p>
                         <div class="hidden sm:block">
                             <button class="px-10 py-3 mt-8 text-sm text-white capitalize bg-black rounded">
                                 {{ $t('btn.shop_now') }}
@@ -40,18 +40,18 @@
                     </div>
                 </div>
                 <!--Second item-->
-                <div class="relative float-left -me-[100%] hidden w-full transition-transform duration-[600ms] ease-in-out motion-reduce:transition-none"
+                <div class="relative float-start -me-[100%] hidden w-full transition-transform duration-[600ms] ease-in-out motion-reduce:transition-none"
                     data-twe-carousel-item>
                     <img src="https://justfields.com/storage/projects/7M5rV059/home-03.jpg"
                         class="block mx-auto w-7xl" />
                     <div
-                        class="absolute inset-y-0 flex flex-col items-center justify-center w-1/3 space-y-2 text-lg font-semibold text-center text-black me-36 end-5">
+                        class="absolute inset-y-0 flex flex-col items-center justify-center w-1/3 space-y-2 text-lg font-semibold text-center text-black me-36 right-12">
                         <p class="text-lg font-semibold capitalize lg:text-4xl">
                             {{ $t('home.lorémica') }}
                         </p>
                         <p class="hidden text-xs font-light line-clamp-2 sm:block">{{
                             $t('home.pellentesque_posuere_morci_lobortis_scelerisque_blandit_donec_an_tellus_lacinia_an_tincidunt_risus_de_cosmopolis_consequat_velitos')
-                        }}</p>
+                            }}</p>
                         <div class="hidden sm:block">
                             <button class="px-10 py-3 mt-8 text-sm text-white capitalize bg-black rounded">
                                 {{ $t('btn.shop_the_collection') }}
@@ -60,18 +60,18 @@
                     </div>
                 </div>
                 <!--Third item-->
-                <div class="relative float-left -me-[100%] hidden w-full transition-transform duration-[600ms] ease-in-out motion-reduce:transition-none"
+                <div class="relative float-start -me-[100%] hidden w-full transition-transform duration-[600ms] ease-in-out motion-reduce:transition-none"
                     data-twe-carousel-item>
                     <img src="https://justfields.com/storage/projects/7M5rV059/home-02.jpg"
                         class="block mx-auto w-7xl" />
                     <div
-                        class="absolute inset-y-0 flex flex-col items-center justify-center w-1/3 space-y-2 text-lg font-semibold text-center text-black ms-36 start-5">
+                        class="absolute inset-y-0 flex flex-col items-center justify-center w-1/3 space-y-2 text-lg font-semibold text-center text-black ms-36 left-12">
                         <p class="text-lg font-semibold capitalize lg:text-4xl">
-                            Beauty Shop
+                            {{ $t('home.beauty_shop') }}
                         </p>
                         <p class="hidden text-xs font-light line-clamp-2 sm:block">{{
                             $t('home.pellentesque_posuere_morci_lobortis_scelerisque_blandit_donec_an_tellus_lacinia_an_tincidunt_risus_de_cosmopolis_consequat_velitos')
-                        }}</p>
+                            }}</p>
                         <div class="hidden sm:block">
                             <button class="px-10 py-3 mt-8 text-sm text-white capitalize bg-black rounded">
                                 {{ $t('btn.shop_now') }}
@@ -85,7 +85,7 @@
             <button
                 class="absolute bottom-0 start-0 top-0 z-[1] flex w-[15%] items-center justify-center border-0 bg-none p-0 text-center text-white opacity-50 transition-opacity duration-150 ease-[cubic-bezier(0.25,0.1,0.25,1.0)] hover:text-white hover:no-underline hover:opacity-90 hover:outline-none focus:text-white focus:no-underline focus:opacity-90 focus:outline-none motion-reduce:transition-none"
                 type="button" data-twe-target="#carouselExampleIndicators" data-twe-slide="prev">
-                <span class="inline-block w-8 h-8">
+                <span class="inline-block w-8 h-8 rtl:rotate-180">
                     <icon name="material-symbols:arrow-back-ios" class="w-6 h-6 text-black" />
                 </span>
                 <span
@@ -95,7 +95,7 @@
             <button
                 class="absolute bottom-0 end-0 top-0 z-[1] flex w-[15%] items-center justify-center border-0 bg-none p-0 text-center text-white opacity-50 transition-opacity duration-150 ease-[cubic-bezier(0.25,0.1,0.25,1.0)] hover:text-white hover:no-underline hover:opacity-90 hover:outline-none focus:text-white focus:no-underline focus:opacity-90 focus:outline-none motion-reduce:transition-none"
                 type="button" data-twe-target="#carouselExampleIndicators" data-twe-slide="next">
-                <span class="inline-block w-8 h-8">
+                <span class="inline-block w-8 h-8 rtl:rotate-180">
                     <icon name="material-symbols:arrow-forward-ios-rounded" class="w-6 h-6 text-black" />
                 </span>
                 <span
